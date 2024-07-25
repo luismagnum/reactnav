b5b1bdc8303e754a6bc7f51b520f3e767a751d0a
luismagnum <luismagnum1@gmail.com>
2024-07-24 23:42:57 -0300

    quitar boton mini

--- src/components/Mini/Mini.jsx
+++ src/components/Mini/Mini.jsx
@@ -6,19 +6,16 @@ const Mini = () => {
   return (
     <div>
       <div>
-        <div className='container max-w-5xl mx-auto px-10 md:px-4 py-2 md:mt-10'>
+        <div className='container max-w-5xl mx-auto px-10 md:px-4 py-2 md:mt-10 mb-8'>
             <div className='flex flex-col-reverse lg:flex-row'>
                 <div className='lg:pr-10 space-y-2'>
                     <div className='text-base text-gray-800 font-medium md:text-base'>50 unidades</div>
                     <div className='text-3xl text-gray-600 font-bold'>Mini-Tequeños</div>
                     <div className='text-base text-gray-800 font-medium mt-2'>Ideales para fiestas y reuniones.</div>
                     <div className='text-justify'>
-                        <p className='text-base text-gray-800 font-medium mt-4'>Congelados y listos para freir.</p>
-                        <p className='text-base text-gray-800 font-medium mt-4'>Instrucciones para cocinar,</p>
-                        <p className='text-base text-gray-800 font-medium mt-4'>Los sirves calienticos.</p>
-                        <button className='text-center items-center text-white bg-primary hover:bg-primaryDark font-bold px-3 py-2 rounded mt-6 mb-10 duration-500'>
-                        <a href="https://wa.me/542984792639?text=Hola quiero hacer un pedido" target="_blank">WhatsApp</a>
-                        </button>
+                        <p className='text-base md:text-2xl text-gray-800 font-medium mt-4'>Congelados y listos para freir.</p>
+                        <p className='text-base md:text-2xl text-gray-800 font-medium mt-4'>Instrucciones para cocinar,</p>
+                        <p className='text-base md:text-2xl text-gray-800 font-medium mt-4'>Los sirves calienticos.</p>
                     </div>
                 </div>
                 <div className='flex-shrink-0 lg:ml-auto mb-4 lg:mb-0'>
