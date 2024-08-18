0a17055fea684aaaedc9db8b78466f8b6297dd71
luismagnum <luismagnum1@gmail.com>
2024-08-18 01:47:28 -0300

    change

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -6,7 +6,6 @@ const Delivery = () => {
     <div>
         <div className='flex flex-col lg:flex-row justify-center items-center lg:px-32 px-5 mx-4 mb-8'>
       <img src={deliv} alt='img2'width={600}/>
-
       <div className='space-y-4 lg:pt-14 mx-4'>
         <h1 className='text-gray-600 font-bold text-3xl text-start'>Delivery Express</h1>
         <p className='text-base md:text-xl text-gray-800 font-medium text-justify'>
