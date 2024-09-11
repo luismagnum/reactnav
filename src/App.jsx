8f56bfef2551a3316a39f87a7f4517a5e0c317a5
luismagnum <luismagnum1@gmail.com>
2024-09-10 23:18:34 -0300

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Boton from './components/Boton'
 import Delivery from './components/Delivery'
 import Empanadas from './components/Empanadas/Empanadas'
 import Footer from './components/footer/Footer'
+import Frecuente from './components/Frecuente/Frecuente'
 import Hero from './components/Hero/Hero'
 import Mini from './components/Mini/Mini'
 import Navar from './components/Navar/Navar'
@@ -10,6 +11,7 @@ import PaymentMethods from './components/PaymentMethods/PaymentMethods'
 import Price from './components/Price/Price'
 import Product from './components/product/Product'
 import Scroll from './components/Scroll'
+import ShoppingCart from './components/ShoppingCart/ShoppingCart'
 import TextSlider from './components/slide/TextSlider'
 
 function App() {
@@ -31,8 +33,10 @@ function App() {
       </div>
       <Scroll />
       <Price />
-      <Delivery />
+      <ShoppingCart />
       <PaymentMethods />
+      <Delivery />
+      <Frecuente />
       <Footer />
       <Boton />
     </main>
--- src/components/PaymentMethods/PaymentMethods.jsx
+++ src/components/PaymentMethods/PaymentMethods.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-
-// Asegúrate de actualizar las rutas según donde guardes las imágenes en tu proyecto
 import mercadoPagoLogo from '../../assets/mercado_pago.png';
 import visaLogo from '../../assets/Visa_Logo.png';
 import masterCardLogo from '../../assets/mastercard_logo.png';
 import debitLogo from '../../assets/debit_card.png';
-import creditLogo from '../../assets/credit_card.png';
 import transferLogo from '../../assets/transfer.png';
 
 const PaymentMethods = () => {
   return (
     <div className="max-w-md mx-auto mt-8 p-4 bg-white shadow-md rounded-lg">
-      <h2 className="text-xl font-bold text-center mb-4">Métodos de Pago</h2>
+      <h2 className="text-gray-800 text-xl font-bold text-center mb-4">Métodos de Pago</h2>
+      <h3 className='text-gray-800 text-xl font-bold text-center mb-2'>Te enviamos Link o Alias</h3>
       <div className="flex flex-wrap justify-center gap-4">
         <img src={visaLogo} alt="Visa" className="h-12 w-auto" />
         <img src={masterCardLogo} alt="MasterCard" className="h-12 w-auto" />
