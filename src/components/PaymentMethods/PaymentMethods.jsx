243f75db7d379fac7a3aafc4351284e9b5b533c3
luismagnum <luismagnum1@gmail.com>
2025-06-17 22:19:16 -0300

    medios de pago

--- src/components/PaymentMethods/PaymentMethods.jsx
+++ src/components/PaymentMethods/PaymentMethods.jsx
@@ -2,8 +2,8 @@ import { SiMercadopago } from "react-icons/si";
 
 export default function PaymentMethods() {
   return (
-    <section className="bg-[#fff]">
-    <div className="max-w-md mx-auto shadow-lg rounded-lg p-6 text-center">
+    <section className="max-w-xl mx-auto p-6 mt-6">
+    <div className="bg-white shadow-2xl rounded-lg p-6 text-center border-t-4 border-yellow-500">
       <h2 className="text-2xl md:text-3xl font-bold text-[#15225b] mb-4">Medios de Pago</h2>
 
       <div className="flex justify-center mb-4 text-blue-900">
